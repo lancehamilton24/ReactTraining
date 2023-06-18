40040e8095c8c5ce42fca0d8a962b434be4e2ef4
Lance Hamilton <developerhamilton@outlook.com>
2023-06-18 18:09:11 -0500

    added component files

---
+++ Tutorial/src/books.js
@@ -0,0 +1,12 @@
+export const books = [{
+    author: 'Bonnie Garmus',
+    title: 'Lessons In Chemistry: A Novel',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg',
+    id: 1
+  },
+  {
+    author: 'Cormac McCarthy',
+    title: 'Blood Meridian: Or the Evening Redness in the West',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/A1l6F+A98ZL._AC_UL600_SR600,400_.jpg',
+    id: 2
+  }]
--- Tutorial/src/index.js
+++ Tutorial/src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { books } from './books';
+import Book from './Book';
 import './index.css';
-
-const books = [{
-  author: 'Bonnie Garmus',
-  title: 'Lessons In Chemistry: A Novel',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg',
-  id: 1
-},
-{
-  author: 'Cormac McCarthy',
-  title: 'Blood Meridian: Or the Evening Redness in the West',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/A1l6F+A98ZL._AC_UL600_SR600,400_.jpg',
-  id: 2
-}]
 
 function BookList() {
   return (
@@ -24,17 +13,6 @@ function BookList() {
     </section>
   );
 }
-
-const Book = (props) => {
-  const { img, title, author } = props.book;
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
-  )
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
