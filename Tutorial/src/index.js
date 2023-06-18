6e7792e4c442fe0dad45d709f716e5a77b293253
Lance Hamilton <developerhamilton@outlook.com>
2023-06-18 03:11:21 -0500

    props

--- Tutorial/src/index.js
+++ Tutorial/src/index.js
@@ -2,32 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const BookList = () => {
+const books = [{
+  author: 'Bonnie Garmus',
+  title: 'Lessons In Chemistry: A Novel',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg',
+  id: 1
+},
+{
+  author: 'Cormac McCarthy',
+  title: 'Blood Meridian: Or the Evening Redness in the West',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/A1l6F+A98ZL._AC_UL600_SR600,400_.jpg',
+  id: 2
+}]
+
+function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book book={book} key={book.id} />;
+      })}
     </section>
-  )
+  );
 }
 
-const Book = () => {
-  const title = 'Lessons In Chemistry: A Novel';
-  const author = 'Bonnie Garmus';
-  const img = 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL600_SR600,400_.jpg';
+const Book = (props) => {
+  const { img, title, author } = props.book;
   return (
     <article className='book'>
-      <img
-        src={img}
-        alt={title}
-      />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
     </article>
   )
-}
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
